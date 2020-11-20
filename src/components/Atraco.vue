1c56262bf065e646fa8c1b6cdbba2bb51c5be895
Khahory <angeld05@live.com>
2020-11-19 21:49:50 -0400

    funcona

---
+++ src/components/Atraco.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <b-button @click="atracar">Atracar!</b-button>
+        <p>Te he atracado: {{num}} veces</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Atraco",
+        data() {
+            return {
+                num: 0
+            }
+        },
+        methods: {
+            atracar(){
+                this.num++
+            }
+        }
+    }
+</script>
---
+++ src/views/Mio.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        Mio
+        <div style="margin: 20px">
+            <Atraco/>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Atraco from "@/components/Atraco";
+
+    export default {
+        name: "Mio",
+        components: {
+            Atraco
+        }
+    }
+</script>
